68ecca7720b7754b3dfc754039e41cfd5024ef9c
zhao <1064742894@qq.com>
2022-09-04 15:58:54 +0800

    new file

--- RayTracing/RayTracing/material.h
+++ RayTracing/RayTracing/material.h
@@ -25,7 +25,7 @@ public:
 };
 class metal : public material {
 public:
-	metal(const vec3& a) : albedo(a) {}
+	metal(const vec3& a, double f) : albedo(a), fuzz(f < 1 ? f : 1) {}
 
 	virtual bool scatter(
 		const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered
@@ -38,4 +38,38 @@ public:
 
 public:
 	vec3 albedo;
+	double fuzz;
+};
+class dielectric : public material {
+public:
+	dielectric(double ri) : ref_idx(ri) {}
+
+	virtual bool scatter(
+		const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered
+	) const {
+		attenuation = vec3(1.0, 1.0, 1.0);
+		double etai_over_etat = (rec.front_face) ? (1.0 / ref_idx) : (ref_idx);
+
+		vec3 unit_direction = unit_vector(r_in.direction());
+		double cos_theta = ffmin(dot(-unit_direction, rec.normal), 1.0);
+		double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+		if (etai_over_etat * sin_theta > 1.0) {
+			vec3 reflected = reflect(unit_direction, rec.normal);
+			scattered = ray(rec.p, reflected);
+			return true;
+		}
+		double reflect_prob = schlick(cos_theta, etai_over_etat);
+		if (random_double() < reflect_prob)
+		{
+			vec3 reflected = reflect(unit_direction, rec.normal);
+			scattered = ray(rec.p, reflected);
+			return true;
+		}
+		vec3 refracted = refract(unit_direction, rec.normal, etai_over_etat);
+		scattered = ray(rec.p, refracted);
+		return true;
+	}
+
+public:
+	double ref_idx;
 };
--- RayTracing/RayTracing/vec3.h
+++ RayTracing/RayTracing/vec3.h
@@ -108,6 +108,17 @@ inline vec3 unit_vector(vec3 v) {
 vec3 reflect(const vec3& v, const vec3& n) {
 	return v - 2 * dot(v, n)*n;
 }
+double schlick(double cosine, double ref_idx) {
+	auto r0 = (1 - ref_idx) / (1 + ref_idx);
+	r0 = r0 * r0;
+	return r0 + (1 - r0)*pow((1 - cosine), 5);
+}
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+	auto cos_theta = dot(-uv, n);
+	vec3 r_out_parallel = etai_over_etat * (uv + cos_theta * n);
+	vec3 r_out_perp = -sqrt(1.0 - r_out_parallel.length_squared()) * n;
+	return r_out_parallel + r_out_perp;
+}
 vec3 random_in_unit_sphere() {
 	while (true) {
 		auto p = vec3::random(-1, 1);
